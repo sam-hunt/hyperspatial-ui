735b87b106cc3a415e1e9f718d975538067db3a4
Gabe <gabriel.riera@sharesight.co.nz>
2022-07-16 19:21:58 +1200

    refactor: add missing type imports

--- src/pages/game/events/spawn-event.ts
+++ src/pages/game/events/spawn-event.ts
@@ -1,5 +1,5 @@
-import { Component } from '../simulation/ecs/component.interface';
-import { AbstractEvent } from './abstract-event';
+import type { Component } from '../simulation/ecs/component.interface';
+import type { AbstractEvent } from './abstract-event';
 
 export interface SpawnEvent extends AbstractEvent {
     event: 'spawn';
